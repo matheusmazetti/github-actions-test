1759e5a4964a28d44cfdbb1997f9afda7c1b9ea2
matheus <matheus.mazetti.22@gmail.com>
2022-08-09 19:53:25 -0300

    testando2

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.post("/students", async (req: Request, res: Response) => {
     data: students,
     skipDuplicates: true
   });
-  //testando...
+  //testando...2
   res.sendStatus(201); 
 });
 
